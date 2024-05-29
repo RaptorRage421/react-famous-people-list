c0885cd369bce78ba2c9b070b703554868db141e
Sam Shacter <sshacter@gmail.com>
2024-05-28 23:10:43 -0500

    completed the required features. going to look at stretch

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './FamousSection.css';
 
 function FamousSection() {
@@ -7,9 +8,18 @@ function FamousSection() {
   let [famousPeopleArray, setPeopleArray] = useState([]);
 
   // TODO: on load, call the fetchPeople() function
-
+  useEffect(()=>{fetchPeople()},[])
+  
   const fetchPeople = () => {
     // TODO: fetch the list of people from the server
+    axios.get('/api/people/')
+    .then((response)=>{
+      setPeopleArray(response.data)
+      console.log("Famous People: ", response.data)
+    })
+    .catch((err)=>{
+      console.error("Error in GET /api/people", err)
+    })
   }
 
   const addPerson = (evt) => {
@@ -20,23 +30,39 @@ function FamousSection() {
 
     // HINT: the server is expecting a person object 
     //       with a `name` and a `role` property
-  
+    axios.post('/api/people/', { name: famousPersonName, role: famousPersonRole })
+    .then(() => {
+      // Refresh famous people list after successful POST request
+      fetchPeople();
+      // Clear input fields
+      setPersonName('');
+      setPersonRole('');
+    })
+    .catch((err) => {
+      console.error("Error in POST /api/people", err);
+    });
+
   }
+
+ 
 
     return (
       <section className="new-person-section">
         <form onSubmit={addPerson}>
           <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => setPersonName(e.target.value)} />
+          <input id="name-input" onChange={e => setPersonName(e.target.value)} value={famousPersonName}/>
           <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
+          <input id="role-input" onChange={e => setPersonRole(e.target.value)} value={famousPersonRole}/>
           <button type="submit">Done</button>
         </form>
         <p>
-          {famousPersonName} is famous for "{famousPersonRole}".
+          <b>{famousPersonName}</b> is famous for <b>"{famousPersonRole}"</b>.
         </p>
         <ul>
           {/* TODO: Render the list of famous people */}
+          {famousPeopleArray.map((person)=>{
+            return (<li key={person.id}><b>{person.name}</b> is famous for <b>{person.role}</b></li>)
+          })}
         </ul>
       </section>
     );
